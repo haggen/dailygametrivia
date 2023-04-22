efc2f049449398ac27780878b92c062a49830291
Arthur <arthur@corenzan.com>
2023-04-22 12:21:15 -0300

    Small refactor

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -18,34 +18,9 @@ import { Lives } from "~/src/components/Lives";
 import { Score } from "~/src/components/Score";
 import { Toast } from "~/src/components/Toast";
 import { Help } from "~/src/components/Help";
+import { getGameOfTheDayOffset } from "~/src/lib/gameOfTheDay";
 
 const maxLives = 10;
-const firstTenPrimeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
-
-function getSeed() {
-  const today = new Date();
-  const y = today.getFullYear();
-  const m = today.getMonth();
-  const d = today.getDate();
-  return parseInt([y, m, d].join(""), 10);
-}
-
-function getRandomIndex(seed: number, listCount: number) {
-  // convert each number from seed to a correspondent prime number
-  const seedPrimeNumbers = seed
-    .toString()
-    .split("")
-    .map((n) => firstTenPrimeNumbers[parseInt(n, 10)]);
-  // multiply all the correspondent prime numbers
-  const seedPrimeProduct = seedPrimeNumbers.reduce((a, b) => a * b);
-  // get the modulo of the product by the list count
-  return seedPrimeProduct % listCount;
-}
-
-function getGameQueryOffset(gameListLength: number) {
-  const seed = getSeed();
-  return getRandomIndex(seed, gameListLength);
-}
 
 export function App() {
   const [{ stage, score, currentLives, guesses }, dispatch] = useSimpleState({
@@ -55,30 +30,30 @@ export function App() {
     guesses: [] as Game[],
   });
 
-  const { data: gameQueryOffset } = useQuery(
+  const { data: gameOfTheDayOffset } = useQuery(
     ["/v4/games/count", { where: defaultGameCriteria }] as const,
     ({ queryKey }) => post<{ count: number }>(...queryKey),
     {
-      select({ count: gameListLength }) {
-        return getGameQueryOffset(gameListLength);
+      select({ count }) {
+        return getGameOfTheDayOffset(count);
       },
     }
   );
 
-  const { data: secretGame } = useQuery(
+  const { data: gameOfTheDay } = useQuery(
     [
       "/v4/games",
       {
         fields: defaultGameFields,
         where: defaultGameCriteria,
         sort: "id",
-        offset: gameQueryOffset,
+        offset: gameOfTheDayOffset,
         limit: 1,
       },
     ] as const,
     ({ queryKey }) => post<Game[]>(...queryKey),
     {
-      enabled: typeof gameQueryOffset === "number",
+      enabled: typeof gameOfTheDayOffset === "number",
       select(game) {
         return fixGameData(game[0]);
       },
@@ -86,10 +61,10 @@ export function App() {
   );
 
   const handleGuess = (guessedGame: Game) => {
-    if (!secretGame) {
+    if (!gameOfTheDay) {
       return;
     }
-    const comparison = compareGames(secretGame, guessedGame);
+    const comparison = compareGames(gameOfTheDay, guessedGame);
 
     if (comparison.id === "exact") {
       dispatch({
@@ -119,7 +94,7 @@ export function App() {
     });
   };
 
-  if (!secretGame) {
+  if (!gameOfTheDay) {
     return <>Loading…</>;
   }
 
@@ -138,7 +113,7 @@ export function App() {
                 <span className={classes.number}>{guesses.length - index}</span>
                 <Guess
                   guess={guessedGame}
-                  comparison={compareGames(secretGame, guessedGame)}
+                  comparison={compareGames(gameOfTheDay, guessedGame)}
                 />
               </li>
             ))}
