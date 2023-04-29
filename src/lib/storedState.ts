4d41571f00cd6430d7589f0f973831ec5ede9a83
Arthur <arthur@corenzan.com>
2023-04-29 16:54:11 -0300

    Prevent stored state issues

--- src/lib/storedState.ts
+++ src/lib/storedState.ts
@@ -1,3 +1,5 @@
+const latestVersion = "1";
+
 export function getStorageKeyOfTheDay() {
   const today = new Date();
   const year = today.getFullYear();
@@ -8,9 +10,13 @@ export function getStorageKeyOfTheDay() {
 
 export function saveState<T>(key: string, state: T) {
   localStorage.setItem(key, JSON.stringify(state));
+  localStorage.setItem("version", latestVersion);
 }
 
 export function loadState<T>(key: string) {
+  if (localStorage.getItem("version") !== latestVersion) {
+    return undefined;
+  }
   const storedState = localStorage.getItem(key);
   if (!storedState) {
     return undefined;
